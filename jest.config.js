544dd1ebaac0acb38485956de1881084aeef4d3f
David Bastos <davidtavares98@gmail.com>
2023-07-05 03:16:17 -0300

    :boom: fix: settings to run the tests

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-default',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'mjs'],
-  testEnvironment: 'node',
-};
